5eadccc19ade94a3ba336e719da048dd446a4bd0
ali raza <aliahmadraza401@gmail.com>
2020-09-17 19:15:38 +0500

    Updates

---
+++ precache-manifest.ec21d46bbe8f696816662aa86b23961e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724b691177ab1d0f995742c6fce3953e",
+    "url": "/Todo-list-App/index.html"
+  },
+  {
+    "revision": "738544d6f04bfb7de497",
+    "url": "/Todo-list-App/static/css/main.3494d464.chunk.css"
+  },
+  {
+    "revision": "60b2c25256c17874a803",
+    "url": "/Todo-list-App/static/js/2.7f5be3f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-list-App/static/js/2.7f5be3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "738544d6f04bfb7de497",
+    "url": "/Todo-list-App/static/js/main.ef10d800.chunk.js"
+  },
+  {
+    "revision": "c7264095c9c02761d0fd",
+    "url": "/Todo-list-App/static/js/runtime-main.4322a612.js"
+  }
+]);
